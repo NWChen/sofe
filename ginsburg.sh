ee2f375c6693741f8997c439d82435b3e96eafbf
Max Aalto <maxaalto@gmail.com>
2023-06-06 19:05:12 -0400

    updating submission script

--- ginsburg.sh
+++ ginsburg.sh
@@ -8,6 +8,7 @@
 #SBATCH --mem-per-cpu=6gb         # The memory the job will use per cpu core
 
 module load anaconda
+module load QE/7.2
 # module load cuda11.1/toolkit
 
 # export LD_LIBRARY_PATH="/burg/home/msa2187/.local/lib/python3.9/site-packages/nvidia/cublas/lib":$LD_LIBRARY_PATH
@@ -15,9 +16,11 @@ export CONDA_ROOT="/burg/opt/anaconda3-2022.05/"
 . $CONDA_ROOT/etc/profile.d/conda.sh
 conda activate sofe
 
-export PYTHONPATH=$PYTHONPATH:/burg/opt/anaconda3-2022.05/lib/python3.9/site-packages:~/.conda/envs/sofe/bin/pip:/burg/urbangroup/users/msa2187/pip-packages
+export PYTHONPATH=$PYTHONPATH:/burg/opt/anaconda3-2022.05/lib/python3.9/site-packages:/burg/urbangroup/users/msa2187/sofe_env/bin/pip:/burg/urbangroup/users/msa2187/sofe_env/lib/python3.9/site-packages
+export ASE_ESPRESSO_COMMAND=/burg/opt/QE/7.2/bin/pw.x -in PREFIX.pwi > PREFIX.pwo
+export ESPRESSO_PSEUDO=/burg/urbangroup/users/msa2187/pseudo
 
 #Command to execute Python program
-/burg/home/msa2187/.conda/envs/sofe/bin/python sputter.py
+/burg/urbangroup/users/msa2187/sofe_env/bin/python sputter.py
 
 #End of script
